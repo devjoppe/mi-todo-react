574f90f3ce4ab7be1c04e509089cab29c0a66882
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-05-01 09:58:45 +0200

    Some basic styling. Its done

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -9,14 +9,16 @@ const AddTodo = ({newPost}:TodoItemProp) => {
     const [todoInput, setTodoInput] = useState('')
     // newPost(todoInput);
     return(
-        <div className="row">
+        <div className="row mt-5">
             <form onSubmit={(e: React.FormEvent) => {
                 e.preventDefault();
                 newPost(todoInput)
                 setTodoInput('')
             }}>
-                <input type="text" value={todoInput} onChange={e => setTodoInput(e.target.value)} required/>
-                <button type="submit">Create todo</button>
+                <div className="input-group mb-3">
+                    <input aria-describedby="button-addon2" className="form-control" type="text" value={todoInput} onChange={e => setTodoInput(e.target.value)} required/>
+                    <button id="button-addon2" className="btn btn-outline-secondary" type="submit">Create todo</button>
+                </div>
             </form>
         </div>
     )
